4337cf34373049a0bfc6bfa1d6617e8e2aa93bab
Elizabeth Kezia Widjaja <e.kezia@gmail.com>
2024-06-03 18:01:58 +0800

    init 🇲🇨

---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+import getArticles from '@/api/get-articles';
+import Image from 'next/image';
+
+export default async function Home() {
+  const _data = await getArticles();
+  if (!_data?.success) {
+    throw 'error';
+  }
+
+  const { success, data: articles } = _data;
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {articles.data.map((article) => {
+        return (
+          <div key={article.id}>
+            <Image
+              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+              src={article.attributes.banner.data.attributes.url}
+              alt={article.attributes.banner.data.attributes.name}
+              width={180}
+              height={37}
+              priority
+            />
+            <div>{article.attributes.title}</div>;
+          </div>
+        );
+      })}
+    </main>
+  );
+}
