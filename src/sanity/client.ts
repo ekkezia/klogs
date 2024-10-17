61bead0dd890cf301af31f59cb0b2d8c94c5ab0c
keziawidjaja <e.kezia@gmail.com>
2024-10-17 11:34:07 +0800

    feat: SMS API routes

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,28 +1,32 @@
 // import 'server-only'
 
-import { createClient, type QueryParams } from 'next-sanity'
+import { createClient, type QueryParams } from "next-sanity"
 
 // const id = nreo0xtv
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: 'production',
-  apiVersion: '2024-01-01',
+  dataset: "production",
+  apiVersion: "2024-01-01",
   useCdn: false,
-});
+})
+
+export const sanityCreate = async (data: any) => {
+  return await client.create(data)
+}
 
 export async function sanityFetch<QueryResponse>({
   query,
   params = {},
   tags,
 }: {
-  query: string;
-  params?: QueryParams;
-  tags?: string[];
+  query: string
+  params?: QueryParams
+  tags?: string[]
 }) {
   return client.fetch<QueryResponse>(query, params, {
     next: {
-      revalidate: process.env.NODE_ENV === 'development' ? 30 : 30,
+      revalidate: process.env.NODE_ENV === "development" ? 30 : 30,
       tags,
     },
-  });
+  })
 }
---
+++ src/types/sanity-types.ts
@@ -0,0 +1,8 @@
+export interface CreateSanityDocument {
+  _type: string
+  name: string
+  email: string
+  date: Date
+  sms: string
+  typoSms: string
+}
